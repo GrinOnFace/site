2cf3055d0ad7433a06de0c01bf6da11342d9ecf8
Renat <renatnabiew@inbox.ru>
2022-08-24 22:14:21 +0300

    'site'

---
+++ women/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank = True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
