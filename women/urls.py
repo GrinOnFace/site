aed7464c1faa15a871e643081ad2f07ba50830ae
Renat <renatnabiew@inbox.ru>
2022-08-25 02:30:02 +0300

    'site

--- women/urls.py
+++ women/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, re_path
 from .views import *
 urlpatterns = [
     path('',index, name = 'home'),
-    path('about/', about, name = 'about')
+    path('about/', about, name = 'about'),
+    path('addpage/', addpage, name = 'add_page'),
+    path('contact/', about, name = 'contact'),
+    path('login/', about, name = 'login'),
+    path('post/<int:post_id>/',show_post,name = 'post')
 ]
--- women/views.py
+++ women/views.py
@@ -2,20 +2,28 @@ from django.http import HttpResponse,HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from .models import *
 
-menu = ['Тимур лох','О сайте', 'Добавить вкладку']
+menu = [{'title': 'О сайте', 'url_name' : 'about'},
+        {'title': 'Добавить в статью', 'url_name' : 'add_page'},
+        {'title': 'Обратная связь', 'url_name' : 'contact'},
+        {'title': 'Войти', 'url_name' : 'login'},]
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts' : posts,'menu' : menu, 'title': 'Главная страница'})
+    context = {'posts' : posts,
+               'menu' : menu,
+               'title': 'Главная страница'}
+    return render(request, 'women/index.html', context = context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu' : menu,'title': 'О сайте'})
 
-def categories(request, catid):
-    print(request.GET)
-    return HttpResponse(f'<h1>Статья по категориям</h1><p>{catid}</p>')
-def archive(request,year):
-    if int(year) > 2020:
-        return redirect('home',permanent = True)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+def contact(request):
+    return HttpResponse('Обратная связь')
+def login(request):
+    return HttpResponse('Авторизвция')
+def show_post(request,post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
+
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
